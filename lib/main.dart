abfb445c20936117d75685247250cc718e896c0a
PudilN <ainurfadhil32@gmail.com>
2025-11-05 21:49:01 +0700

    Tutorial 7

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:football_news_mobile/menu.dart';
+import 'package:football_news_mobile/screens/menu.dart';
 
 void main() {
   runApp(const MyApp());
@@ -30,8 +30,8 @@ class MyApp extends StatelessWidget {
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
         colorScheme: ColorScheme.fromSwatch(
-          primarySwatch: Colors.green,
-        ).copyWith(secondary: Colors.greenAccent[400]),
+          primarySwatch: Colors.blue,
+        ).copyWith(secondary: Colors.blueAccent[400]),
       ),
       home: MyHomePage(),
     );
